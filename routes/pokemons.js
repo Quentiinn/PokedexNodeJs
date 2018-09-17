e70f14a3135c3de2a06a50fd79701cc96aab2781
quentin <quentiin-25@live.fr>
2018-09-17 20:12:32 +0200

    ajout de la vue detail pokemon

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -7,4 +7,12 @@ router.get('/' , (req,res) => {
 	});
 })
 
+router.get('/:id' , (req,res) => {
+	//recupere l'id de la barre d'adresse
+	Pokemon.findById(req.params.id).populate('types').then(pokemon => {
+		res.render('pokemons/show.html' , {pokemons:pokemon})
+	},
+	err => res.status(500).send(err));
+})
+
 module.exports = router;
